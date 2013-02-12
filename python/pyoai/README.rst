85e1221b80c7678d427bf226adc593cb0eecc533
Rob Cermak <rob.cermak@gmail.com>
2013-02-12 10:32:50 -0900

    Update README, add 2013 to LICENSE, add fork of pyoai

---
+++ python/pyoai/README.rst
@@ -0,0 +1,30 @@
+.. -*- restructuredtext -*-
+
+OAIPMH
+======
+
+The oaipmh module is a Python implementation of an "Open Archives
+Initiative Protocol for Metadata Harvesting" (version 2) client and
+server. The protocol is described here:
+
+http://www.openarchives.org/OAI/openarchivesprotocol.html
+
+Below is a simple implementation of an OAIPMH client:
+
+>>> from oaipmh.client import Client
+>>> from oaipmh.metadata import MetadataRegistry, oai_dc_reader
+
+>>> URL = 'http://uni.edu/ir/oaipmh'
+
+>>> registry = MetadataRegistry()
+>>> registry.registerReader('oai_dc', oai_dc_reader)
+>>> client = Client(URL, registry)
+
+>>> for record in client.listRecords(metadataPrefix='oai_dc'):
+>>>    print record
+
+
+The pyoai package also contains a generic server implementation of the 
+OAIPMH protocol, this is used as the foundation of the `MOAI Server Platform`_
+
+.. _MOAI Server Platform: http://pypi.python.org/pypi/MOAI
---
+++ python/pyoai/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='pyoai',
+    version='2.4.5dev',
+    author='Infrae',
+    author_email='info@infrae.com',
+    url='http://www.infrae.com/download/oaipmh',
+    classifiers=["Development Status :: 4 - Beta",
+                 "Programming Language :: Python",
+                 "License :: OSI Approved :: BSD License",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Environment :: Web Environment"],
+    description="""\
+The oaipmh module is a Python implementation of an "Open Archives
+Initiative Protocol for Metadata Harvesting" (version 2) client and server.
+The protocol is described here:
+
+http://www.openarchives.org/OAI/openarchivesprotocol.html
+""",
+    long_description=(open(join(dirname(__file__), 'README.rst')).read()+
+        '\n\n'+
+        open(join(dirname(__file__), 'HISTORY.txt')).read()),
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    zip_safe=False,
+    license='BSD',
+    keywords='OAI-PMH xml archive',
+    install_requires=['lxml'],
+)
