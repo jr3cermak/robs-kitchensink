bcdcc1e2db1ed3bb555f5e5d2d5256557f707c95
Rob Cermak <rob.cermak@gmail.com>
2013-04-05 16:45:24 -0800

    Add a great great circle distance routine

---
+++ python/utils/distance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import math
+
+# http://www.platoscave.net/blog/2009/oct/5/calculate-distance-latitude-longitude-python/
+# Haversine formula example in Python
+# Author: Wayne Dyck
+
+# Calculate great circle distance between two lat/lon points
+##
+def distance(origin, destination):
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
+
